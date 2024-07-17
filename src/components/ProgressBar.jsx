5e034efd90e1730f5ad3275c0cfe5698f40d3a01
Besa Salimusaj <besasalimusaj98@gmail.com>
2024-07-17 16:58:10 +0200

    adding placepicker

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+
+export default function ProgressBar({timer}){
+const [remaningTime, setRemaningTime] = useState(timer);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemaningTime((prevTime) => prevTime - 10);
+    }, 10);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  return <progress value={remaningTime} max={timer} />
+}
